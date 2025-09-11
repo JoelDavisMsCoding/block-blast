f19847dd661d801392a5b7d97fdc301f00988af3
JoelDavisMsCoding <166549396+JoelDavisMsCoding@users.noreply.github.com>
2025-09-10 20:07:35 -0500

    testing mobile

--- src/GameBoard.jsx
+++ src/GameBoard.jsx
@@ -73,7 +73,7 @@ function GameBoard({
           } else if (isGhostCell(rowIndex, colIndex)) {
             cellClasses.push("ghost", `ghost-color-${currentPiece.colorId}`);
             if (dragSource === "touch") {
-              cellStyle.transform = "translateY(-120%) scale(1.1)";
+              cellStyle.transform = "translateY(-150%) scale(1.1)";
               cellStyle.zIndex = 2000;
             }
           }
